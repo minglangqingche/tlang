145abbfad457d174f246249351adb805170bfe25
minglangqingche <2472159834@qq.com>
2023-06-25 20:46:24 +0800

    master-> 优化了项目结构.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 pub mod tlang {
-    mod read_file;
+    mod read_file {
+        use std::fs;
+
+        pub fn read_file(path: &str) -> String {
+            match fs::read_to_string(&path) {
+                Ok(val) => {
+                    val
+                },
+                Err(_) => {
+                    crate::interpreter_error::error_exit(&format!("can't read file ' {} '!", path), 64);
+                    "".to_string()
+                }
+            }
+        }
+    }
 
     pub mod vm {
         use super::read_file;
